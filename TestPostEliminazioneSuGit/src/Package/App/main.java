426d89c6a217421004ff219a78c73d07a1d4458c
Valerio1805 <valeriodonnini@hotmail.it>
2020-05-06 15:45:48 +0200

    Modifica nel secondo branch

--- TestPostEliminazioneSuGit/src/Package/App/main.java
+++ TestPostEliminazioneSuGit/src/Package/App/main.java
@@ -5,6 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Tentativo di collegare un nuovo progetto sulla stessa repo di prima
+		//Modifica nel secondo branch
 	}
 
 }
