9c0265e1823f0523be416b62251e520830d7f767
Valerio1805 <valeriodonnini@hotmail.it>
2020-05-06 15:43:13 +0200

    Prima prova

---
+++ TestPostEliminazioneSuGit/src/Package/App/main.java
@@ -0,0 +1,10 @@
+package Package.App;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Tentativo di collegare un nuovo progetto sulla stessa repo di prima
+	}
+
+}
