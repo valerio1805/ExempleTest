2e1ba9d6b8ff432f5d3073bbf49e247a0adf08d6
Valerio1805 <valeriodonnini@hotmail.it>
2020-05-06 15:14:28 +0200

    Collego il nuovo progetto alla repo di github

---
+++ TestDiGitHub/src/TetDiGitHub/App/main.java
@@ -0,0 +1,12 @@
+package TetDiGitHub.App;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Import repo di github
+		//Collego il nuovo progetto alla repo di github
+		
+	}
+
+}
