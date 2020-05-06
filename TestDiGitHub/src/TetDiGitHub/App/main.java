736f278febcebe3de3525d9f14369d70a35a6253
Valerio1805 <valeriodonnini@hotmail.it>
2020-05-06 15:19:50 +0200

    Commit aggiornamento

--- TestDiGitHub/src/TetDiGitHub/App/main.java
+++ TestDiGitHub/src/TetDiGitHub/App/main.java
@@ -9,6 +9,7 @@ public class main {
 		//Modifico il file e faccio un nuovo commit		
 		//Modifico il file su eclipse, ma anche su github -> cosa succede se faccio il push da Eclipse?
 		//Faccio una seconda modifica su Eclipse poi provo a pushare tutto su Eclipses
+		//Push non consentito -> non avevo l'ultima versione del codice sul mio Eclipse
 	}
 
 }
